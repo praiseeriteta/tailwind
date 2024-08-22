bcc5445a4e696fe7558ae94425a3712c89052a4b
praiseeriteta <eriteta@gmail.com>
2024-08-22 12:37:25 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./Build/*.html"],
+  theme: {
+    extend: {
+      colors:{
+        gskills:{
+          400:'#2E073F',
+          300:'#7A1CAC',
+          200:'#AD49E1',
+          100:'#EBD3F8',
+      },
+    },
+    backgroundColor:{
+      pboi:{
+        400:'#00712D',
+        300:'#D5ED9F',
+        200:'#FFFBE6',
+        100:'#FF9100',
+      },
+    },a
+  },
+},
+  plugins: [],
+}
